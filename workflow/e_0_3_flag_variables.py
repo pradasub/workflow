ac1d3075046d850d7458955d0718586098e96a26
Prachanda Subedi <Prachanda.Subedi@ey.com>
2019-11-30 11:29:36 -0500

    Changed biasness hardcoding in flag_variables file

--- workflow/e_0_3_flag_variables.py
+++ workflow/e_0_3_flag_variables.py
@@ -11,7 +11,7 @@ def describe():
 
 
 
-def flag_vars(df,n_unique_for_bias_check=10,frac = 1,perc_null_gtr = 50.):
+def flag_vars(df,bias_ratio = 0.9,frac=1,perc_null_gtr = 50.,n_unique_for_bias_check=10):
     
     list_cols = list(df.columns)
     df = df.sample(frac=frac)
@@ -27,7 +27,7 @@ def flag_vars(df,n_unique_for_bias_check=10,frac = 1,perc_null_gtr = 50.):
         if df[i].nunique() <= n_unique_for_bias_check:
             value_count = df[i].value_counts()
             biasness = value_count.values[0]/value_count.sum()
-            if biasness>0.9:
+            if biasness>bias_ratio:
                 n_unique=df[i].nunique()
                 bias_n_unique[i] = []
                 bias_n_unique[i].append(value_count.index[0])
