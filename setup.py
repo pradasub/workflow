4caa9e4f73c89fdd8973536cb294aa75c6275e95
Prachanda Subedi <Prachanda.Subedi@ey.com>
2020-01-19 11:51:04 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='python_workflow',
+      version='1.0',
+      description='Python workflow for data science projects',
+      url='https://github.com/pradasub/test_workflows',
+      author='Dr Prada',
+      author_email='prachandasubedi@gmail.com',
+      license='prop',
+      packages=['workflow'],
+      zip_safe=False)
